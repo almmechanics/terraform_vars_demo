309b1937670dc81e314cff2eac0dae124fe31fa5
Martin Palmer <martin.palmer@almmechanics.com>
2020-05-05 22:59:23 +0100

    reconfigured for az

--- module/invoke-az.ps1
+++ module/invoke-az.ps1
@@ -3,6 +3,8 @@ param()
 
 Set-StrictMode -Version Latest
 
-gci env: | ForEach-Object {write-verbose ('{0}={1}' -f $_.Name,$_.Value)}
+# gci env: | ForEach-Object {write-verbose ('{0}={1}' -f $_.Name,$_.Value)}
+$azReply = (az version) | ConvertFrom-Json -AsHashtable
 
-return @{'az' = 'response'} | convertto-json
+
+ return @{'az' = $azReply['azure-cli']} | ConvertTo-Json
