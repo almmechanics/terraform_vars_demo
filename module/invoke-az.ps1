78980f9288fe7eef773d8710cb6c90eb3fb6a27b
Martin Palmer <martin.palmer@almmechanics.com>
2020-05-05 23:33:52 +0100

    update variable names

--- module/invoke-az.ps1
+++ module/invoke-az.ps1
@@ -1,16 +1,24 @@
 [CmdletBinding()] 
-param(
+param(    
     [string]
     $subscription,
     [string]
     $tenant
 )
 
-Set-StrictMode -Version Latest
-
+# # Set-StrictMode -Version Latest
+# $VerbosePreference = continue
+# Write-Verbose $subscription
+# Write-Verbose $tenant
 # gci env: | ForEach-Object {write-verbose ('{0}={1}' -f $_.Name,$_.Value)}
 
 $azAccount = (az account show)| ConvertFrom-Json -AsHashtable
-$loggedIn = (($azAccount.id -eq $subscription) -and ($azAccount.tenantId -eq $tenant))
-
+if ($azAccount)
+{
+    $loggedIn = (($azAccount.id -eq $subscription) -and ($azAccount.tenantId -eq $tenant))
+}
+else
+{
+    $loggedIn = "Null"
+}
  return @{'az' = "$loggedIn"} | ConvertTo-Json
